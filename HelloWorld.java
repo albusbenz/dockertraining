c068c4f150e5045b0c37aeb2c1c43c5561bad456
albusbenz <alberto.benini@gmail.com>
2016-03-22 16:38:11 +0000

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World! My name is ++BenZ");
+		System.out.println("Bella zio pippo! My name is ++BenZ");
 	}
 }
